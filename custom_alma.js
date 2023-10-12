69cad4b1d036c1ad5127fb4c774fec21ec275b94
smachefert <sylvain.machefert@u-bordeaux-montaigne.fr>
2023-10-12 19:14:21 +0200

    Version 1.3

--- custom_alma.js
+++ custom_alma.js
@@ -1,45 +1,74 @@
-function waitForElm(selector) {
+function waitForElm(selector, childList = true, subtree = true) {
   return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(mutations => {
       if (document.querySelector(selector)) {
-          return resolve(document.querySelector(selector));
+        observer.disconnect();
+        resolve(document.querySelector(selector));
       }
+    });
 
-      const observer = new MutationObserver(mutations => {
-          if (document.querySelector(selector)) {
-              observer.disconnect();
-              resolve(document.querySelector(selector));
-          }
-      });
-
-      observer.observe(document.body, {
-          childList: true,
-          subtree: true
-      });
+    observer.observe(document.body, {
+      attributes: true,
+      childList: childList,
+      subtree: subtree
+    });
   });
 }
+
+function addWarningMessage() {
+  console.log("LoanDeskA : " + loanDesk);
+
+  if (loanDesk !== true) {
+    console.log("Ajout message averitissement");
+    const checkoutNode = document.querySelector("div#CheckoutItem");
+    checkoutNode.innerHTML += "<div class='checkout-warning'>Attention, vous êtes sur un bureau de <strong>consultation sur place</strong>. N'oubliez pas de repasser en prêt/retour pour des transactions classiques.</div>";
+  } else {
+    console.log("Pas de message averitissement car loandesk KO");
+  }
+}
+
+const observer = new MutationObserver(list => {
+  bodyId = document.body.id;
+  console.log("Affichage page : " + bodyId);
+  console.log("LoanDeskZ : " + loanDesk);
+
+  if (bodyId == "body_id_xml_file_loan.fulfillment_discharge_item.xml") {
+    addWarningMessage();
+  } else if (bodyId == "body_id_xml_file_loan.fulfillment_patron_workspace_loan_new_ui.xml") {
+    addWarningMessage();
+  }
+});
+
+observer.observe(document.body, { attributes: true, childList: false, subtree: false, characterData: false });
+
+function checkPlace(locationNode) {
+  // Remove all div.checkout-warning
+  document.querySelectorAll('div.checkout-warning').forEach(e => e.remove());
   
-waitForElm('span.location-pinable-menu-text-container').then((elm) => {
   const header = document.querySelector("div.main-layout-navHeader");
+  const text = locationNode.textContent;
+  if (text.includes("sur place")) {
+    header.classList.add("main-menu_warning");
+    loanDesk = false;
+    addWarningMessage();
+    console.log("LoanDeskB : " + loanDesk);
+  } else {
+    header.classList.remove("main-menu_warning");
+    loanDesk = true;
+    console.log("LoanDeskC : " + loanDesk);
+  }
+}
+
+waitForElm('span.location-pinable-menu-text-container').then((elm) => {
   const locationNode = document.querySelector("span.location-pinable-menu-text-container").querySelector("span");
-  /* console.log(location);
-  const node = document.evaluate(
-    "//span[contains(@class, 'location-pinable-menu-text-container')]/span",
-    document,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null,
-    ).singleNodeValue;
-    */
-    function checkPlace(locationNode) {
-      const text = locationNode.textContent;
-      if (text.includes("sur place")) { 
-        header.classList.add("main-menu_warning");
-      } else {
-        header.classList.remove("main-menu_warning");
-      }
-    }
-    const observer = new MutationObserver(list => {
-      checkPlace(locationNode);
-    });
-    observer.observe(locationNode, {attributes: false, childList: true, subtree: true, characterData: true});
+  checkPlace(locationNode);
+  
+  const observer = new MutationObserver(list => {
+    checkPlace(locationNode);
+  });
+  observer.observe(locationNode, { attributes: false, childList: true, subtree: true, characterData: true });
 });
